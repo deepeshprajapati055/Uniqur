7328e462b80583696926f11ed5f8cae3dc189285
Deepesh Prajapati <prajapatideepesh055@gmail.com>
2025-03-02 22:59:44 +0530

    index page is ready

--- src/main/java/com/uniqur/controller/HomeController.java
+++ src/main/java/com/uniqur/controller/HomeController.java
@@ -11,4 +11,13 @@ public class HomeController {
 		return "index";
 	}
 	
+	@GetMapping("/login")
+	public String loginPage() {
+		return "login";
+	}
+	
+	@GetMapping("/register")
+	public String registrationPage() {
+		return "register";
+	}
 }
